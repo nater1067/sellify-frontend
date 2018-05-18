bf11a93f46df05ce682b92b8050f3a66948dc8de
Nate Turner <nater1992@gmail.com>
2018-05-18 07:48:49 -0700

    receive events using http2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  step1Complete = false;
+  step2Complete = false;
+  step3Complete = false;
+  step4Complete = false;
+  step5Complete = false;
+  step6Complete = false;
+  step7Complete = false;
+  step8Complete = false;
+  step9Complete = false;
+  step10Complete = false;
+  step11Complete = false;
+
+  constructor() { }
+
+  ngOnInit() {
+
+
+    if (typeof(EventSource) !== 'undefined') {
+      console.info('Http2 Supported.');
+      var source = new EventSource("http://localhost:8080/events");
+      source.onmessage = function(event) {
+        console.log(event);
+        // document.getElementById("result").innerHTML += event.data + "<br>";
+      };
+    } else {
+      console.info('Http2 Not Supported.');
+      // Sorry! No server-sent events support..
+    }
+
+    let currentStep = 0;
+    const interval = setInterval(() => {
+
+      currentStep++;
+      this[`step${currentStep}Complete`] = true;
+
+      if (currentStep === 11) {
+        clearInterval(interval);
+      }
+    }, 2000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatStepperModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatStepperModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
